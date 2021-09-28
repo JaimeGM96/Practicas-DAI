2f495d53e0667bfb8426d63f354a107da6e2eecc
Jaime Gallardo Mateo <jaimegallardo@correo.ugr.es>
2021-09-28 11:31:36 +0200

    Ejercicio 3

---
+++ Practica 1/ejercicios/ejercicio3.py
@@ -0,0 +1,15 @@
+# La criba de Eratóstenes
+def criba_eratostenes(x):
+    numeros = list(range(2, x+1))
+    i = 0
+
+    while numeros[i]**2 <= x:
+        for n in numeros:
+            if n != numeros[i]:
+              if n % numeros[i] == 0:
+                numeros.remove(n)
+        
+        i += 1
+    return numeros
+
+print(criba_eratostenes(200))
