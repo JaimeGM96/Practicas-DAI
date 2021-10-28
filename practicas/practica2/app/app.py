54f968674196f6eab8f7bde0fbd2b2689f0899f2
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-10-28 10:39:18 +0200

    Añadida ruta pagina login

--- practicas/practica2/app/app.py
+++ practicas/practica2/app/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
   return render_template('index.html')
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('error.html')
