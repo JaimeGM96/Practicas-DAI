9dcfc38c857d600ba7b96bb6ee4102914fe29573
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-11-01 16:49:16 +0100

    Pagina login

--- practicas/practica2/app/app.py
+++ practicas/practica2/app/app.py
@@ -1,16 +1,26 @@
 #./app/app.py
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, url_for, session, redirect, flash
 import random, re
+
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
           
 # Páginas
 @app.route('/')
-def hello_world():
+def index():
   return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
+  error=None
+  if request.method == 'POST':
+    if request.form['username'] != 'admin' or \
+            request.form['password'] != 'admin':
+        error = 'Invalid credentials'
+    else:
+        flash('You were successfully logged in')
+        return redirect(url_for('index'))
+  return render_template('login.html', error=error)
 
 @app.errorhandler(404)
 def page_not_found(error):
