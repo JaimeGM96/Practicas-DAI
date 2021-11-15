af946eb0015d9bd56b1edb4a163c44f0d15e20b8
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-11-15 10:27:05 +0100

    Atualizada practica 3 con practica 2 terminada

--- practicas/practica3/app/app.py
+++ practicas/practica3/app/app.py
@@ -1,48 +1,96 @@
 #./app/app.py
 from flask import Flask, render_template, request, url_for, session, redirect, flash
 import random, re
+from pickleshare import *
 
 app = Flask(__name__)
 app.secret_key = 'clave-secreta-para-el-uso-de-sesiones'
+db = PickleShareDB('db_pickleshare/')
           
 # Páginas
 @app.route('/')
 def index():
   return render_template('index.html')
 
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+  if request.method == 'POST':
+    db['username'] = request.form['username']
+    db['password'] = request.form['password']
+    flash('Te has registrado correctamente')
+    return redirect(url_for('index'))
+
+  return render_template('registro.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   error=None
   if request.method == 'POST':
-    usuario = request.form['username']
-    if usuario != 'admin' or \
-            request.form['password'] != 'admin':
+    username = request.form['username']
+    password = request.form['password']
+    if username != db['username'] or \
+            password != db['password']:
         error = 'Invalid credentials'
     else:
-        flash('You were successfully logged in')
-        session['username'] = usuario
+        flash('Bienvenido ' + username)
+        session['username'] = username
         return redirect(url_for('index'))
   return render_template('login.html', error=error)
 
-@app.route('/ordena')
+@app.route('/logout')
+def logout():
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+@app.route('/ordena', methods=['GET', 'POST'])
 def ordena():
-  return render_template('ordena.html')
+  resultado = None
+  if request.method == 'POST':
+    datos = request.form['datos']
+    resultado = list(map(int, datos.split(",")))
+    ordena_seleccion(resultado)
 
-@app.route('/eratostenes')
+  return render_template('ejercicios.html', resultado=resultado)
+
+@app.route('/eratostenes', methods=['GET', 'POST'])
 def eratostenes():
-  return render_template('eratostenes.html')
+  resultado = None
+  if request.method == 'POST':
+    datos = request.form['datos']
+    resultado = criba_eratostenes(int(datos))
 
-@app.route('/fibonacci')
+  return render_template('ejercicios.html', resultado=resultado)
+
+@app.route('/fibonacci', methods=['GET', 'POST'])
 def fibonacci():
-  return render_template('fibonacci.html')
+  resultado = None
+  if request.method == 'POST':
+    datos = request.form['datos']
+    resultado = fibonacci(int(datos))
 
-@app.route('/corchetes')
+  return render_template('ejercicios.html', resultado=resultado)
+
+@app.route('/corchetes', methods=['GET', 'POST'])
 def corchetes():
-  return render_template('corchetes.html')
+  resultado = None
+  if request.method == 'POST':
+    datos = request.form['datos']
+    resultado = generar_cadena(int(datos))
 
-@app.route('/expresiones_regulares')
+  return render_template('ejercicios.html', resultado=resultado)
+
+@app.route('/expresiones_regulares', methods=['GET', 'POST'])
 def expresiones_regulares():
-  return render_template('expresiones_regulares.html')
+  resultado = None
+  if request.method == 'POST':
+    datos = request.form['datos']
+    resultado = validar_expresiones(datos)
+    if resultado:
+      resultado = "cadena válida"
+    else:
+      resultado = "cadena no válida"
+
+  return render_template('ejercicios.html', resultado=resultado)
 
 @app.errorhandler(404)
 def page_not_found(error):
