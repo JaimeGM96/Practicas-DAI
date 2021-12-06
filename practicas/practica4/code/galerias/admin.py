93141cdfc7e464cfeab945edeacae5037e3d86dc
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-12-06 14:31:01 +0100

    Añadida funcionalidad a los forms y registrados en admin.py

--- practicas/practica4/code/galerias/admin.py
+++ practicas/practica4/code/galerias/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Galeria, Cuadro
 
-# Register your models here.
+admin.site.register(Galeria)
+admin.site.register(Cuadro)
--- practicas/practica4/code/galerias/views.py
+++ practicas/practica4/code/galerias/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
+from .models import Galeria, Cuadro
+from .forms import GaleriaForm, CuadroForm
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'index.html')
 
 def test_template(request):
+    if request.method == 'POST':
+        form = CuadroForm(request.POST)
+
+        if form.is_valid():
+            cuadro = form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = CuadroForm()
+
     context = {
-        'saludo': 'Juan'
-    }   # Aquí van la las variables para la plantilla
-    return render(request,'test.html', context)
+        'form': form
+    }
+
+    return render(request, 'test.html', context)
+
+def nueva_galeria(request):
+    if request.method == 'POST':
+        form = GaleriaForm(request.POST)
+
+        if form.is_valid():
+            galeria = form.save(commit=False)
+            galeria.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = GaleriaForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'test.html', context)
