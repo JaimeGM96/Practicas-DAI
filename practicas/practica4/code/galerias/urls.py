f9c458e8945db5f87e40e973d7b945a25d124b0f
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-12-12 12:16:12 +0100

    Añadidas las urls de las nuevas vistas

--- practicas/practica4/code/galerias/urls.py
+++ practicas/practica4/code/galerias/urls.py
@@ -4,6 +4,14 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('test_template', views.test_template, name='test_template'),
+  path('lista_galerias',views.lista_galerias, name='lista_galerias'),
+  path('<id>/detalle_galeria', views.detalle_galeria, name= 'detalle_galeria'),
+  path('lista_cuadros',views.lista_cuadros, name='lista_cuadros'),
+  path('<id>/detalle_cuadro', views.detalle_cuadro, name= 'detalle_cuadro'),
   path('nueva_galeria', views.nueva_galeria, name='nueva_galeria'),
   path('nuevo_cuadro', views.nuevo_cuadro, name='nuevo_cuadro'),
+  path('<id>/modificar_galeria', views.modificar_galeria, name='modificar_galeria'),
+  path('modificar_cuadro', views.modificar_cuadro, name='modificar_cuadro'),
+  path('borrar_galeria', views.borrar_galeria, name='borrar_galeria'),
+  path('borrar_cuadro', views.borrar_cuadro, name='borrar_cuadro'),
 ]
