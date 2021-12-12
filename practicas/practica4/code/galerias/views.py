8dc1212483645922ed56611331722b4caf9af200
JaimeGM96 <jaimegallardo@correo.ugr.es>
2021-12-12 12:16:44 +0100

    Añadidas funciones para mostrar los datos

--- practicas/practica4/code/galerias/views.py
+++ practicas/practica4/code/galerias/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.http import HttpResponseRedirect
 from .models import Galeria, Cuadro
 from .forms import GaleriaForm, CuadroForm
@@ -7,11 +7,14 @@ def index(request):
     return render(request, 'index.html')
 
 def test_template(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and 'modificado' in request.POST:
         form = CuadroForm(request.POST)
 
         if form.is_valid():
-            cuadro = form.save()
+            nombre = request.POST.get('nombre')
+            galeria = request.POST.get('galeria')
+            autor = request.POST.get('autor')
+            Cuadro.objects.filter(nombre=nombre).update(nombre=nombre, galeria=galeria, autor=autor)
             return HttpResponseRedirect('/')
     else:
         form = CuadroForm()
@@ -21,6 +24,34 @@ def test_template(request):
     }
 
     return render(request, 'test.html', context)
+
+def lista_galerias(request):
+    context = {}
+
+    context["dataset"] = Galeria.objects.all()
+
+    return render(request, "lista_galerias.html", context)
+
+def lista_cuadros(request):
+    context = {}
+
+    context["dataset"] = Cuadro.objects.all()
+
+    return render(request, "lista_cuadros.html", context)
+
+def detalle_galeria(request, id):
+    context = {}
+
+    context["data"] = Galeria.objects.get(id=id)
+
+    return render(request, "detalle_galeria.html", context)
+
+def detalle_cuadro(request, id):
+    context = {}
+
+    context["data"] = Cuadro.objects.get(id=id)
+
+    return render(request, "detalle_cuadro.html", context)
 
 def nueva_galeria(request):
     if request.method == 'POST':
